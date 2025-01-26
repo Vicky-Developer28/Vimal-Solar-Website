3dcb2d6845995ef18d77e4beada10f2d4e61fb7a
Vicky-Developer28 <vicky28.developer@gmail.com>
2025-01-26 16:13:06 +0530

    Test

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
