6fa459e8df5b1b6a237b8f255152c91f7dca1057
Vicky-Developer28 <vicky28.developer@gmail.com>
2025-01-29 22:31:58 +0530

    test for css

--- app/app/settings.py
+++ app/app/settings.py
@@ -106,8 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-# Static and Media files settings
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
