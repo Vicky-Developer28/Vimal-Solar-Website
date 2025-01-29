ef68d895b4171379439c878683d1918aed26f5d7
Vicky-Developer28 <vicky28.developer@gmail.com>
2025-01-29 22:46:13 +0530

    Create a super user

--- app/manage.py
+++ app/manage.py
@@ -20,3 +20,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+from django.contrib.auth import get_user_model
+import os
+
+def create_superuser():
+    User = get_user_model()
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser created!")
+    else:
+        print("Superuser already exists.")
+
+create_superuser()
